8474c3c77b9909a674ae74ffc4fc74f1070b321b
Banashree81 <62277816+Banashree81@users.noreply.github.com>
2020-12-09 10:17:22 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -21,9 +21,9 @@ class Dustbin {
       push();        
       rectMode(CENTER);
       fill(180);
-      rect(this.leftwall.position.x-5, this.leftwall.position.y, this.width, this.height);
-      rect(this.rightwall.position.x+5, this.rightwall.position.y, this.width, this.height);9
-      rect(this.lowerWall.position.x, this.lowerWall.position.y, this.width+130, this.height-70);
+      //rect(this.leftwall.position.x-5, this.leftwall.position.y, this.width, this.height);
+      //rect(this.rightwall.position.x+5, this.rightwall.position.y, this.width, this.height);9
+      //rect(this.lowerWall.position.x, this.lowerWall.position.y, this.width+130, this.height-70);
       image(this.image, this.leftwall.position.x-20, this.lowerWall.position.y-145, 200,150)
       
       pop();
